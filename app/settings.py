a6c4408526df920c67c60bd2970e7a4f224a12bf
Levenko Evgeny <levenko.evgeny@yandex.by>
2022-09-03 23:47:29 +0300

    fix CORS

--- app/settings.py
+++ app/settings.py
@@ -47,6 +47,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:3000",
     "http://127.0.0.1:8000",
     "http://127.0.0.1:3000",
+    "https://word-trainer.herokuapp.com"
 ]
 
 ROOT_URLCONF = 'app.urls'
