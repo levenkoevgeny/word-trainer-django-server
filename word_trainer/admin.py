e261a88d250b0e9f35f503fcc8fb9b81fe97ecb2
Evgeny Levenko <51111834+levenkoevgeny@users.noreply.github.com>
2022-09-19 13:59:38 +0300

    admin import export

--- word_trainer/admin.py
+++ word_trainer/admin.py
@@ -1,6 +1,21 @@
 from django.contrib import admin
 from .models import MyUser, Dictionary, Word
+from import_export import resources
+from import_export.admin import ImportExportModelAdmin
 
 admin.site.register(MyUser)
 admin.site.register(Dictionary)
-admin.site.register(Word)
+#admin.site.register(Word)
+
+class WordResource(resources.ModelResource):
+
+    class Meta:
+        model = Word
+        fields = ('id', 'dictionary', 'word_rus', 'word_eng')
+
+
+class WordAdmin(ImportExportModelAdmin):
+    resource_class = WordResource
+
+
+admin.site.register(Word, WordAdmin)
